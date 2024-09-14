f1d80158db62b721bbabddf5ff9fedba12e2ec35
SimonGuzman <siguzman@unicauca.edu.co>
2024-09-14 16:44:14 -0500

    Waste variables

--- my-food/src/main/java/com/unicauca/my_food/domain/waste/value_objects/CauseWaste.java
+++ my-food/src/main/java/com/unicauca/my_food/domain/waste/value_objects/CauseWaste.java
@@ -1,5 +1,12 @@
 package com.unicauca.my_food.domain.waste.value_objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CauseWaste {
-    
+    private String description;
 }
--- my-food/src/main/java/com/unicauca/my_food/domain/waste/value_objects/ProductWaste.java
+++ my-food/src/main/java/com/unicauca/my_food/domain/waste/value_objects/ProductWaste.java
@@ -1,5 +1,15 @@
 package com.unicauca.my_food.domain.waste.value_objects;
 
+import com.unicauca.my_food.domain.inventory.Product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductWaste {
-    
+    private Product productId;
+    private String nameProduct;
 }
