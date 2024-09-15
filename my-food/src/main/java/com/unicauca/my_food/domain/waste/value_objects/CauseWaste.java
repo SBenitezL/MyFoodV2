5da8c141414213a62d836d73001ddbd9884ce808
SimonGuzman <siguzman@unicauca.edu.co>
2024-09-15 16:04:23 -0500

    implementacion metodos para el desperdicio

--- my-food/src/main/java/com/unicauca/my_food/domain/waste/value_objects/CauseWaste.java
+++ my-food/src/main/java/com/unicauca/my_food/domain/waste/value_objects/CauseWaste.java
@@ -1,12 +1,33 @@
 package com.unicauca.my_food.domain.waste.value_objects;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.List;
+import java.util.UUID;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+import com.unicauca.my_food.domain.waste.constants.CauseWasteConstants;
+
+import lombok.Getter;
+
+@Getter
 public class CauseWaste {
+    private String id;
     private String description;
+    private List<String> causes;
+
+    public CauseWaste(int causeIndex){
+        this.id = UUID.randomUUID().toString();
+        this.description = selectCause(causeIndex);
+        this.causes = CauseWasteConstants.getAllCauses();
+    }
+
+    public String selectCause(int causeIndex){
+        if(causeIndex > 0 && causeIndex <= causes.size()){
+            return causes.get(causeIndex - 1);
+        }else{
+            return CauseWasteConstants.CAUSE_WASTE_ERROR;
+        }
+    }
+
+    public boolean validCause(String cause){
+        return causes.contains(cause);
+    }
 }
